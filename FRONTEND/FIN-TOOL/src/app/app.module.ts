1352ec0d5ac70b24453e3479569c2dec3fa64eae
Derik Vila <derikvila@gmail.com>
2021-07-10 17:28:43 -0400

    sidenav

--- FRONTEND/FIN-TOOL/src/app/app.module.ts
+++ FRONTEND/FIN-TOOL/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidenavComponent } from './components/template/sidenav/sidenav.component'; 
+import {MatListModule} from '@angular/material/list'; 
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidenavComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
